a8203b7b6b7fb244cfea13d8e86b671824d166cc
Zombie <Honor@ZOMBIE>
2023-02-07 21:27:49 +0600

    Add reads multiplies Urls

--- AutoParser/Models/GoogleSheetSettingsModel.cs
+++ AutoParser/Models/GoogleSheetSettingsModel.cs
@@ -12,21 +12,6 @@
         public string? RankingStarsItemPropName { get; set; }
         public string? Selector { get; set; }
         public string? SelectorEnd { get; set; }
-        public WorkUrl? WorkUrl { get; set; }
-    }
-
-    public class WorkUrl
-    {
-        public string? Url0 { get; set; }
-        public string? Url1 { get; set; }
-        public string? Url2 { get; set; }
-        public string? Url3 { get; set; }
-        public string? Url4 { get; set; }
-        public string? Url5 { get; set; }
-        public string? Url6 { get; set; }
-        public string? Url7 { get; set; }
-        public string? Url8 { get; set; }
-        public string? Url9 { get; set; }
-        public string? Url10 { get; set; }
+        public string[] WorkUrl { get; set; }
     }
 }
--- AutoParser/Program.cs
+++ AutoParser/Program.cs
@@ -1,31 +1,18 @@
 ﻿using AutoParser.Helpers;
 using AutoParser.WebDriver;
 
-//Console.WriteLine("Start parsing");
-//
+Console.WriteLine("Start parsing");
+
 ApiWebDriver _apiWebDriver = new ApiWebDriver();
-//
-//List<string> urls = new List<string>();
-//
-////TODO make input Add short
-//urls.Add(JsonReader.GetValues().WorkUrl.Url1);
-//urls.Add(JsonReader.GetValues().WorkUrl.Url2);
-//urls.Add(JsonReader.GetValues().WorkUrl.Url3);
-//urls.Add(JsonReader.GetValues().WorkUrl.Url4);
-//urls.Add(JsonReader.GetValues().WorkUrl.Url5);
-//urls.Add(JsonReader.GetValues().WorkUrl.Url6);
-//urls.Add(JsonReader.GetValues().WorkUrl.Url7);
-//urls.Add(JsonReader.GetValues().WorkUrl.Url8);
-//urls.Add(JsonReader.GetValues().WorkUrl.Url9);
-//urls.Add(JsonReader.GetValues().WorkUrl.Url10);
-//
-//foreach (var item in urls)
-//{
-//    _apiWebDriver.RunDriverClient(item);
-//    Console.WriteLine("Parser is working");
-//}
-//Console.WriteLine("Is Ready:");
 
-_apiWebDriver.ListenGoogleSheets();
+List<string> urls = JsonReader.GetValues().WorkUrl.ToList();
 
-//Console.ReadLine();
+foreach (var url in urls)
+{
+    _apiWebDriver.RunDriverClient(url);
+    Console.WriteLine(url);
+}
+
+Console.WriteLine("Is Ready:");
+
+Console.ReadLine();
